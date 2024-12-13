eb8416b75046d80d290d85103f1c30d6bc24b4e5
Nicolás Martínez Van der Looven <legodosmildos@gmail.com>
2024-12-13 11:44:02 +0100

    Arreglado algunos errores y completado scipts

--- deploy_spark_task.sh
+++ deploy_spark_task.sh
@@ -2,23 +2,28 @@
 # Set script to fail on any errors
 set -e
 
-read -p "Enter input text for word2vec: " input_text || exit 1
+read -p "Enter input textfile name you want to use for Word2Vec: " input_text || exit 1
 
 input_name="${input_text%.*}"
 output_csv="${input_name}.csv"
 output_image="${input_name}.png"
 
+echo
 echo "Input text file used: $input_text"
 echo "Output vectors csv file used: $output_csv"
 echo "Output vectors image file used: $output_image"
+echo
 
-read -p "Do you want to restart all services before starting the Spark Task? (Y/N): " confirm || exit 1
+read -p "Do you want to restart all services before starting the Spark task? (Y/N): " \
+  confirm || exit 1
 
 start_services_flag=false
 if [[ "$confirm" =~ ^[yY]$ || "$confirm" =~ ^[yY][eE][sS]$ ]]; then
     start_services_flag=true
 fi
 
-ansible-playbook -i inventory.ini -u hadoop spark_task.yml -T 30 --extra-vars "input_text=${input_text} output_csv=${output_csv} output_image=${output_image} start_services_flag=${start_services_flag}"
+ansible-playbook -i inventory.ini -u hadoop spark_task.yml -T 30 \
+  --extra-vars "input_text=${input_text} output_csv=${output_csv} output_image=${output_image} \
+  start_services_flag=${start_services_flag}"
 
 gio open $output_image
--- full_deploy.sh
+++ full_deploy.sh
@@ -5,21 +5,21 @@ set -e
 
 # Start Vagrant (equivalent to vagrant up)
 echo "Starting Vagrant..."
-vagrant up || exit 1
+vagrant up
 
 # Remove old SSH keys for hadoop@master and workers
 echo "Removing old SSH keys..."
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "master" 2>/dev/null || true
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "worker1" 2>/dev/null || true
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "worker2" 2>/dev/null || true
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "worker3" 2>/dev/null || true
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "master" >/dev/null 2>&1 || true
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "worker1" >/dev/null 2>&1 || true
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "worker2" >/dev/null 2>&1 || true
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "worker3" >/dev/null 2>&1 || true
 
 # Re-add SSH keys by establishing connections
 echo "Re-adding SSH keys for nodes..."
-ssh-keyscan -H master >> "$HOME/.ssh/known_hosts"
-ssh-keyscan -H worker1 >> "$HOME/.ssh/known_hosts"
-ssh-keyscan -H worker2 >> "$HOME/.ssh/known_hosts"
-ssh-keyscan -H worker3 >> "$HOME/.ssh/known_hosts"
+ssh-keyscan master >> "$HOME/.ssh/known_hosts" 2>/dev/null
+ssh-keyscan worker1 >> "$HOME/.ssh/known_hosts" 2>/dev/null
+ssh-keyscan worker2 >> "$HOME/.ssh/known_hosts" 2>/dev/null
+ssh-keyscan worker3 >> "$HOME/.ssh/known_hosts" 2>/dev/null
 
 # Execute the configuration playbook
 echo "Executing Ansible playbook for Hadoop and Spark configuration..."
@@ -28,17 +28,23 @@ ansible-playbook -i inventory.ini -u hadoop configuration.yml -T 30
 echo "Process completed successfully!"
 
 # Execute the word2vec task on Spark node if user confirms
-read -p "Continue with executing Word2Vec task on Spark nodes? (Y/N): " confirm && [[ "$confirm" =~ ^[yY]$ || "$confirm" =~ ^[yY][eE][sS]$ ]] || exit 1
+read -p "Continue with executing Word2Vec task on Spark nodes? (Y/N): " \
+  confirm && [[ "$confirm" =~ ^[yY]$ || "$confirm" =~ ^[yY][eE][sS]$ ]] || exit 1
+echo "Skipping service restart..."
+
 read -p "Enter input textfile name you want to use for Word2Vec: " input_text || exit 1
 
 input_name="${input_text%.*}"
 output_csv="${input_name}.csv"
 output_image="${input_name}.png"
 
+echo
 echo "Input text file used: $input_text"
 echo "Output vectors csv file used: $output_csv"
 echo "Output vectors image file used: $output_image"
+echo
 
-ansible-playbook -i inventory.ini -u hadoop spark_task.yml -T 30 --extra-vars "input_text=${input_text} output_csv=${output_csv} output_image=${output_image}"
-
+ansible-playbook -i inventory.ini -u hadoop spark_task.yml -T 30 \
+  --extra-vars "input_text=${input_text} output_csv=${output_csv} output_image=${output_image} \
+  start_services_flag=false"
 gio open $output_image
