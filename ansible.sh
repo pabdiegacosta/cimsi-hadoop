6de44d99a39aef75ff3f24b7cd0db0810afb21c5
Nicolás Martínez Van der Looven <legodosmildos@gmail.com>
2024-12-13 11:43:57 +0100

    Modificaciones viernes

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#ansible-playbook -i inventory.ini -u hadoop -K all.yml -T 30
-ansible-playbook -i inventory.ini -u hadoop -K spark.yml -T 30
+ansible-playbook -i inventory.ini -u hadoop -K all.yml -T 30
+#ansible-playbook -i inventory.ini -u hadoop -K spark.yml -T 30
 #ansible-playbook -i inventory.ini -u hadoop -K aux.yml -T 30
